eca9a5097246b1c7600f821afe66b4a98053bf76
Arman <armanzarei1378@gmail.com>
2021-05-22 22:01:02 +0430

    Bootstrap register form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,37 +1,39 @@
 @extends('base')
 
 @section('body')
-    <div class="row">
-        <div class="col-3 m-auto">
-            <div class="card mt-4">
-                <div class="card-header">
-                    <i class="fal fa-sign-in"></i> Login
-                </div>
-                <div class="card-body">
-                    @if ($errors->any())
-                        @foreach ($errors->all() as $error)
-                            <div class="alert alert-danger">
-                                <i class="fal fa-engine-warning"></i> {{ $error }}
-                            </div>
-                        @endforeach
+    <div class="container">
+        <div class="row">
+            <div class="col-5 m-auto">
+                <div class="card mt-4">
+                    <div class="card-header">
+                        <i class="fal fa-sign-in"></i> Login
+                    </div>
+                    <div class="card-body">
+                        @if ($errors->any())
+                            @foreach ($errors->all() as $error)
+                                <div class="alert alert-danger">
+                                    <i class="fal fa-engine-warning"></i> {{ $error }}
+                                </div>
+                            @endforeach
 
-                    @endif
-                    <form action="{{ route('login.action') }}" method="POST">
-                        @csrf
-                        <div class="mb-3">
-                            <label for="exampleInputEmail1" class="form-label">Email address</label>
-                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email">
-                        </div>
-                        <div class="mb-3">
-                            <label for="exampleInputPassword1" class="form-label">Password</label>
-                            <input type="password" class="form-control" id="exampleInputPassword1" name="password">
-                        </div>
-                        {{--<div class="mb-3 form-check">
-                            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                            <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                        </div>--}}
-                        <button type="submit" class="btn btn-outline-primary">Submit</button>
-                    </form>
+                        @endif
+                        <form action="{{ route('login.action') }}" method="POST">
+                            @csrf
+                            <div class="mb-3">
+                                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email" value="{{ old('email') }}">
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleInputPassword1" class="form-label">Password</label>
+                                <input type="password" class="form-control" id="exampleInputPassword1" name="password">
+                            </div>
+                            {{--<div class="mb-3 form-check">
+                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                            </div>--}}
+                            <button type="submit" class="btn btn-outline-primary">Submit</button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,23 +1,52 @@
 @extends('base')
 
 @section('body')
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
+    <div class="container">
+        <div class="row">
+            <div class="col-5 m-auto">
+                <div class="card mt-4">
+                    <div class="card-header">
+                        <i class="fal fa-user-plus"></i> Register
+                    </div>
+                    <div class="card-body">
+                        @if ($errors->any())
+                            @foreach ($errors->all() as $error)
+                                <div class="alert alert-danger">
+                                    <i class="fal fa-engine-warning"></i> {{ $error }}
+                                </div>
+                            @endforeach
+                        @endif
+                        <form action="{{ route('register.action') }}" method="POST">
+                            @csrf
+                            <div class="mb-3">
+                                <label for="email" class="form-label">Email address</label>
+                                <input type="email" class="form-control" id="email" name="email" value="{{ old('email') }}">
+                            </div>
+                            <div class="mb-3">
+                                <label for="firstname" class="form-label">Firstname</label>
+                                <input type="text" class="form-control" id="firstname" name="firstname" value="{{ old('firstname') }}">
+                            </div>
+                            <div class="mb-3">
+                                <label for="lastname" class="form-label">Lastname</label>
+                                <input type="text" class="form-control" id="lastname" name="lastname" value="{{ old('lastname') }}">
+                            </div>
+                            <div class="mb-3">
+                                <label for="password" class="form-label">Password</label>
+                                <input type="password" class="form-control" id="password" name="password">
+                            </div>
+                            <div class="mb-3">
+                                <label for="passwordConfirmation" class="form-label">Password Confirmation</label>
+                                <input type="password" class="form-control" id="passwordConfirmation" name="password_confirmation">
+                            </div>
+                            <div class="mb-3">
+                                <label for="mobile" class="form-label">Telephone</label>
+                                <input type="tel" class="form-control" id="mobile" name="mobile" value="{{ old('mobile') }}">
+                            </div>
+                            <button type="submit" class="btn btn-outline-primary">Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
-    @endif
-    <form action="{{ route('register.action') }}" method="POST">
-        @csrf
-        <input type="email" name="email" placeholder="email">
-        <input type="text" name="firstname" placeholder="firstname">
-        <input type="text" name="lastname" placeholder="lastname">
-        <input type="password" name="password" placeholder="password">
-        <input type="password" name="password_confirmation" placeholder="password">
-        <input type="tel" name="mobile">
-        <input type="submit" value="Submit">
-    </form>
+    </div>
 @endsection
