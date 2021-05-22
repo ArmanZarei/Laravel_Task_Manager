3215b463a07adf9f11f9fcd3bb4ceb9c30a3fb09
Arman <armanzarei1378@gmail.com>
2021-05-22 16:37:38 +0430

    Add base blade layout

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,31 +1,23 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-@if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
-<form action="{{ route('register.action') }}" method="POST">
-    @csrf
-    <input type="email" name="email" placeholder="email">
-    <input type="text" name="firstname" placeholder="firstname">
-    <input type="text" name="lastname" placeholder="lastname">
-    <input type="password" name="password" placeholder="password">
-    <input type="password" name="password_confirmation" placeholder="password">
-    <input type="tel" name="mobile">
-    <input type="submit" value="Submit">
-</form>
-</body>
-</html>
+@extends('base')
+
+@section('body')
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    <form action="{{ route('register.action') }}" method="POST">
+        @csrf
+        <input type="email" name="email" placeholder="email">
+        <input type="text" name="firstname" placeholder="firstname">
+        <input type="text" name="lastname" placeholder="lastname">
+        <input type="password" name="password" placeholder="password">
+        <input type="password" name="password_confirmation" placeholder="password">
+        <input type="tel" name="mobile">
+        <input type="submit" value="Submit">
+    </form>
+@endsection
---
+++ resources/views/base.blade.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    @yield('meta')
+
+    <title>Task Manager</title>
+
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/fontawesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
+    @yield('css')
+
+</head>
+<body>
+
+@yield('body')
+
+<script src="{{ asset('js/jquery-3.6.0.min.js') }}"></script>
+<script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+@yield('js')
+
+</body>
+</html>
