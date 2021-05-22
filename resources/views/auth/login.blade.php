657d17243c77a0bde41af47dff0953a03a57c6b7
Arman <armanzarei1378@gmail.com>
2021-05-22 21:43:55 +0430

    Bootstrap login form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,19 +1,39 @@
 @extends('base')
 
 @section('body')
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
+    <div class="row">
+        <div class="col-3 m-auto">
+            <div class="card mt-4">
+                <div class="card-header">
+                    <i class="fal fa-sign-in"></i> Login
+                </div>
+                <div class="card-body">
+                    @if ($errors->any())
+                        @foreach ($errors->all() as $error)
+                            <div class="alert alert-danger">
+                                <i class="fal fa-engine-warning"></i> {{ $error }}
+                            </div>
+                        @endforeach
+
+                    @endif
+                    <form action="{{ route('login.action') }}" method="POST">
+                        @csrf
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">Email address</label>
+                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label">Password</label>
+                            <input type="password" class="form-control" id="exampleInputPassword1" name="password">
+                        </div>
+                        {{--<div class="mb-3 form-check">
+                            <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                        </div>--}}
+                        <button type="submit" class="btn btn-outline-primary">Submit</button>
+                    </form>
+                </div>
+            </div>
         </div>
-    @endif
-    <form action="{{ route('login.action') }}" method="POST">
-        @csrf
-        <input type="email" name="email" placeholder="email">
-        <input type="password" name="password" placeholder="password">
-        <input type="submit" value="Submit">
-    </form>
+    </div>
 @endsection
