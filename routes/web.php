1468ddb5e5397a1296535c4cb35c455464e12f36
Arman <armanzarei1378@gmail.com>
2021-05-23 15:59:09 +0430

    Add auth and guest middlewares to some routes

--- routes/web.php
+++ routes/web.php
@@ -16,20 +16,25 @@ use App\Http\Controllers\Auth\ForgotPasswordController;
 |
 */
 
-Route::get('/', [TaskController::class, 'index'])->name('tasks.index');
-Route::resource('tasks', TaskController::class)->only(['store', 'update', 'destroy']);
 
-Route::view('/register', 'auth.register')->name('register');
-Route::post('/register', [AuthController::class, 'register'])->name('register.action');
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/', [TaskController::class, 'index'])->name('tasks.index');
+    Route::resource('tasks', TaskController::class)->only(['store', 'update', 'destroy']);
+});
 
-Route::get('/email/verify/{id}/{hash}', [AuthController::class, 'verify'])->name('verification.verify');
+Route::group(['middleware' => 'guest'], function () {
+    Route::view('/register', 'auth.register')->name('register');
+    Route::post('/register', [AuthController::class, 'register'])->name('register.action');
 
-Route::view('/login', 'auth.login')->name('login');
-Route::post('/login', [AuthController::class, 'login'])->name('login.action');
+    Route::get('/email/verify/{id}/{hash}', [AuthController::class, 'verify'])->name('verification.verify');
 
-Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::view('/login', 'auth.login')->name('login');
+    Route::post('/login', [AuthController::class, 'login'])->name('login.action');
 
-Route::view('/forgot-password', 'auth.forget_password')->name('password.forgot')->middleware('guest');
-Route::post('/forgot-password', [ForgotPasswordController::class, 'forgotPassword'])->name('password.request')->middleware('guest');
-Route::get('/reset-password/{token}', [ForgotPasswordController::class, 'resetPassword'])->name('password.reset');
-Route::post('/reset-password', [ForgotPasswordController::class, 'updatePassword'])->name('password.update');
+    Route::view('/forgot-password', 'auth.forget_password')->name('password.forgot')->middleware('guest');
+    Route::post('/forgot-password', [ForgotPasswordController::class, 'forgotPassword'])->name('password.request')->middleware('guest');
+    Route::get('/reset-password/{token}', [ForgotPasswordController::class, 'resetPassword'])->name('password.reset');
+    Route::post('/reset-password', [ForgotPasswordController::class, 'updatePassword'])->name('password.update');
+});
+
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth');
